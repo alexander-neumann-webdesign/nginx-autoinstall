087b35a9d07d7578497d483c8e0481367d801d46
Alexander Neumann <alexander.paul.neumann@gmail.com>
2024-11-16 00:46:07 +0100

    new version, tested

--- nginx-headless-minimal.sh
+++ nginx-headless-minimal.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#usage: chmod +x ./nginx-headless-minimal.sh && ./nginx-headless-minimal.sh
 
 if [[ $EUID -ne 0 ]]; then
 	echo -e "Sorry, you need to run this as root"
@@ -6,8 +7,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Define versions
-NGINX_STABLE_VER=${NGINX_STABLE_VER:-1.24.0}
-NGINX_MAINLINE_VER=${NGINX_MAINLINE_VER:-1.25.2}
+NGINX_STABLE_VER=${NGINX_STABLE_VER:-1.26.2}
+NGINX_MAINLINE_VER=${NGINX_MAINLINE_VER:-1.27.2}
 
 # Choose between NGINX_STABLE_VER and NGINX_MAINLINE_VER
 NGINX_VER=$NGINX_MAINLINE_VER
@@ -40,6 +41,7 @@ NGINX_MODULES=${NGINX_MODULES:-"--with-threads \
 	--with-file-aio \
 	--with-http_ssl_module \
 	--with-http_v2_module \
+	--with-http_v3_module \
 	--with-http_mp4_module \
 	--with-http_auth_request_module \
 	--without-http_mirror_module \
@@ -61,11 +63,16 @@ mkdir -p /usr/local/src/nginx/modules
 
 # Dependencies
 apt-get update
-apt-get install -y build-essential ca-certificates wget curl libpcre3 libpcre3-dev autoconf unzip automake libtool tar git libssl-dev zlib1g-dev uuid-dev lsb-release libxml2-dev libxslt1-dev cmake
+apt-get install -y build-essential ca-certificates wget curl libpcre3 libpcre3-dev autoconf unzip automake libtool tar git libssl-dev zlib1g-dev uuid-dev lsb-release libxml2-dev libxslt1-dev cmake equivs
 
 #Brotli
 if [[ $BROTLI == 'y' ]]; then
-	echo "building module: ngx_brotli ..."
+	echo ""
+	echo "# ---------------------------------------"
+	echo "# Building Module: ----------------------"
+	echo "# ngx_brotli ----------------------------"
+	echo "# ---------------------------------------"
+	echo ""
 	
 	# cd /usr/local/src/nginx/modules || exit 1
 	# git clone https://github.com/google/ngx_brotli
@@ -75,8 +82,8 @@ if [[ $BROTLI == 'y' ]]; then
 	cd /usr/local/src/nginx/modules || exit 1
 	git clone --recurse-submodules -j8 https://github.com/google/ngx_brotli
 	
-	mkdir ngx_brotli/deps/brotli/out
-	cd ngx_brotli/deps/brotli/out
+	mkdir /usr/local/src/nginx/modules/ngx_brotli/deps/brotli/out
+	cd /usr/local/src/nginx/modules/ngx_brotli/deps/brotli/out
 	
 	cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_CXX_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_INSTALL_PREFIX=./installed ..
 	cmake --build . --config Release --target brotlienc
@@ -92,7 +99,12 @@ fi
 
 # Cache Purge
 if [[ $CACHEPURGE == 'y' ]]; then
-	echo "building module: ngx_cache_purge ..."
+	echo ""
+	echo "# ---------------------------------------"
+	echo "# Building Module: ----------------------"
+	echo "# ngx_cache_purge -----------------------"
+	echo "# ---------------------------------------"
+	echo ""
 
 	cd /usr/local/src/nginx/modules || exit 1
 	git clone --depth 1 https://github.com/nginx-modules/ngx_cache_purge
@@ -115,7 +127,21 @@ if [[ ! -e /etc/nginx/nginx.conf ]]; then
 	wget https://raw.githubusercontent.com/alexander-neumann-webdesignnginx-autoinstall/master/conf/nginx.conf
 fi
 
+# add nginx user
+echo ""
+echo "# ---------------------------------------"
+echo "# Adding nginx User ---------------------"
+echo "# ---------------------------------------"
+echo ""
+groupadd nginx
+adduser --system --no-create-home --shell /bin/false --group --disabled-login nginx
+
 # compile nginx
+echo ""
+echo "# ---------------------------------------"
+echo "# Compiling Nginx  ----------------------"
+echo "# ---------------------------------------"
+echo ""
 cd /usr/local/src/nginx/nginx-${NGINX_VER} || exit 1
 ./configure $NGINX_OPTIONS $NGINX_MODULES
 make -j "$(nproc)"
@@ -131,6 +157,7 @@ if [[ ! -e /lib/systemd/system/nginx.service ]]; then
 	cd /lib/systemd/system/ || exit 1
 	wget https://raw.githubusercontent.com/alexander-neumann-webdesign/nginx-autoinstall/master/conf/nginx.service
 	# Enable nginx start at boot
+	systemctl start nginx
 	systemctl enable nginx
 fi
 
@@ -160,19 +187,52 @@ systemctl restart nginx
 
 # Block Nginx from being installed via APT
 if [[ $(lsb_release -si) == "Debian" ]] || [[ $(lsb_release -si) == "Ubuntu" ]]; then
+	echo ""
+	echo "# ---------------------------------------"
+	echo "# Block Nginx  --------------------------"
+	echo "# from being installed via APT ----------"
+	echo "# ---------------------------------------"
+	echo ""
 	cd /etc/apt/preferences.d/ || exit 1
 	echo -e 'Package: nginx*\nPin: release *\nPin-Priority: -1' >nginx-block
 fi
 
-# Resolve apt dependencies
+# Create fake package for nginx to avoid dependencies
 if [[ $(lsb_release -si) == "Debian" ]] || [[ $(lsb_release -si) == "Ubuntu" ]]; then
-	apt-get install equivs
-	cd /usr/local/src/nginx/
-	mkdir equivs && cd equivs
-	echo -e 'Section: misc\nPriority: optional\nStandards-Version: 3.9.2\n\nPackage: nginx\nVersion: ${NGINX_VER}\nMaintainer: alexanderneumann\nArchitecture: all\nDescription: Fake package for nginx to avoid dependencies' > nginx-dummy.ctl
+	echo ""
+	echo "# ---------------------------------------"
+	echo "# Create fake equivs package ------------"
+	echo "# to avoid dependencies -----------------"
+	echo "# ---------------------------------------"
+	echo ""
+	cd /usr/local/src/nginx/ || exit 1
+	mkdir equivs-nginx
+	cd equivs-nginx
+	equivs-control nginx
+	cat >nginx <<EOF
+Section: misc
+Priority: optional
+Standards-Version: 3.9.2
+
+Package: nginx
+Version: $NGINX_VER
+Maintainer: alexanderneumann
+Architecture: all
+Description: Fake package for nginx to avoid dependencies
+EOF
+	equivs-build nginx
+	dpkg -i nginx_${NGINX_VER}_all.deb
+
 fi
 
 # We're done !
-echo "Installation done."
+echo ""
+echo "# ---------------------------------------"
+echo "# Installation done. --------------------"
+echo "# ---------------------------------------"
+echo ""
+nginx -t
+systemctl status nginx
+
 exit
 ;;
